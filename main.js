9b2a91a9bdde8b892f8c52a734b48088da2ce320
cxalem <smarketing.cuz@gmail.com>
2022-01-26 18:53:21 -0500

    Now you can add ideas to the dashboard

--- main.js
+++ main.js
@@ -1,40 +1,81 @@
 const ideas = [];
 const userNames = [];
 const userIdeas = [];
-const app = document.querySelector(".app-container");
+const appContainer = document.querySelector(".app-container");
 const submitBtn = document.querySelector(".main-button");
-const postForm = document.querySelector(".post-form");
 const mainForm = document.querySelector('.main-form');
+const app = document.getElementById('app');
+const container = document.querySelector('.container');
+const postBtn = document.querySelector('.post-button');
+const postForm = document.getElementById('ideas-form');
 
-function getNameIdeas() {
+const getNameIdeasFor = () => {
   const yourName = document.createElement("h2");
   const nameSource = document.getElementById("your-name");
   const h2Container = document.createElement("div");
 
-  const ideasSource = document.getElementById("ideas-for");
+  const ideaSource = document.getElementById("ideas-for");
   const ideas = document.createElement("h3");
   const h3Container = document.createElement("div");
 
   yourName.textContent = `Hi, ${nameSource.value}`;
   h2Container.appendChild(yourName);
 
-  ideas.textContent = `Ideas for ${ideasSource.value}`;
+  ideas.textContent = `Ideas for ${ideaSource.value}`;
   h3Container.appendChild(ideas);
 
-  if (ideasSource.value == null ||
-    ideasSource.value.length == 0
-  ) {
-    ideasSource.classList.add("error", "shake");
+  if (ideaSource.value == null ||
+    ideaSource.value.length == 0) {
+    ideaSource.classList.add("error", "shake");
   } else if (nameSource.value.length == 0 || nameSource.value == null) {
     nameSource.classList.add("error", "shake");
   } else {
-    userNames.push(h2Container);
-    userIdeas.push(h3Container);
-    app.insertAdjacentElement("afterbegin", ...userNames);
-    app.append(...userIdeas);
-    app.style.visibility = 'visible';
-    mainForm.style.display = 'none';
+    userNames.push(nameSource.value);
+    userIdeas.push(ideaSource.value);
+    appContainer.insertAdjacentElement("afterbegin", h2Container);
+    appContainer.insertBefore(h3Container, app);
+    appContainer.style.visibility = 'visible';
+    container.removeChild(mainForm);
   }
 }
 
-submitBtn.addEventListener("click", getNameIdeas);
+const addIdea = () => {
+  const ideaContainer = document.createElement('div');
+  ideaContainer.classList.add('idea-container');
+  const name = document.createElement('h4');
+  const idea = document.createElement('span');
+
+  name.textContent = userNames;
+  idea.textContent = postForm.value;
+
+  if (postForm.value == null ||
+    postForm.value.length == 0) {
+    postForm.classList.add("error", "shake");
+  } else {
+    likeBtn();
+    ideaContainer.append(name, idea, likeBtn);
+
+    console.log(likeBtn)
+  
+    ideas.push(ideaContainer);
+    app.append(...ideas);
+
+    postForm.value = '';
+  }
+}
+
+const likeBtn = () => {
+  const likeBtn = document.createElement('button');
+  const heart = document.createElement('img');
+  const likeSpan = document.createElement('span');
+
+  heart.src = './img/heart.svg';
+  likeSpan.textContent = 0;
+  likeBtn.classList.add('like-btn');
+
+  likeBtn.append(heart, likeSpan);
+
+}
+
+submitBtn.addEventListener("click", getNameIdeasFor);
+postBtn.addEventListener('click', addIdea);
